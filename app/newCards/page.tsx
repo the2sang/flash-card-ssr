665050f94ef65a6d958bf4fe4fab044f45be0b8c
the2sang <the2sang@gmail.com>
2023-05-05 14:55:55 +0900

    first mildstone

--- app/newCards/page.tsx
+++ app/newCards/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useState, useEffect } from "react";
-
 import Card from "@/components/Card";
 import {MemoryCard} from "@/types/types";
 
--- components/MyCards.tsx
+++ components/MyCards.tsx
@@ -2,22 +2,64 @@
 import { AnimatePresence, motion } from "framer-motion"
 import Card from "@/components/Card";
 import useCards from "@/hook/useCards";
-import React from "react";
+import React, {useState} from "react";
+import {MemoryCard} from "@/types/types";
 
 const MyCards = () => {
   const { data, isLoading, error } = useCards()
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>Something went wrong 😅</div>
+  // https://www.debuggr.io/react-map-of-undefined/
+  const cards = data?.list.map((card: MemoryCard) => {
+    return <Card card={card} key={card.id} />;
+  });
+
+  const loading = <div className="loading">Loading flashcard content...</div>;
+
+  // navigation in cards
+  const [current, setCurrent] = useState(0);
+  function previousCard() {
+    setCurrent(current - 1);
+  }
+  function nextCard() {
+    setCurrent(current + 1);
+  }
 
   return (
-    <motion.div className="my-12 flex  flex-col-reverse">
-      <AnimatePresence>
-        {data?.list.map((card) => (
-          <Card card={card} key={card.id} />
-        ))}
-      </AnimatePresence>
-    </motion.div>
-  )
+    <div>
+      {/* number of cards */}
+      {data && data.list.length > 0 ? (
+        <div className="cardNumber">
+          Card {current + 1} of {data.list.length}
+        </div>
+      ) : (
+        ""
+      )}
+      {/* /number of cards */}
+
+      {/* render cards */}
+      {data && data.list.length > 0 ? cards[current] : loading}
+      {/* /render cards */}
+
+      {/* render nav buttons */}
+      <div className="nav">
+        {current > 0 ? (
+          <button onClick={previousCard}>Previous card</button>
+        ) : (
+          <button className="disabled" disabled>
+            Previous card
+          </button>
+        )}
+        {current < data.list.length - 1 ? (
+          <button onClick={nextCard}>Next card</button>
+        ) : (
+          <button className="disabled" disabled>
+            Next card
+          </button>
+        )}
+        {/* /render nav buttons */}
+      </div>
+    </div>
+  );
 }
+
 export default MyCards
