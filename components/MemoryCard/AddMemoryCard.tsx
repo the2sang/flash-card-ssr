174debacbea486450c80c7f36d5b286b169b269e
JANA <the2sang@gmail.com>
2023-05-08 09:18:51 +0900

    add MemoryCard Component from Comp

---
+++ components/MemoryCard/AddMemoryCard.tsx
@@ -0,0 +1,165 @@
+"use client";
+
+import { AiOutlinePlus } from "react-icons/ai";
+import Modal from "@/components/Modal";
+import React, {FormEventHandler, useRef, useState} from "react";
+import {addMemoryCard, editMemoryCard} from "@/app/api/memoryCardApi";
+import { useRouter} from "next/navigation";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {TMemoryCard} from "@/types/types";
+// import {v4 as uuidv4} from "uuid";
+
+interface CategoryProps {
+  id: string | undefined;
+  mainCategory: TMemoryCard
+}
+
+const AddMemoryCard = () => {
+
+  const nameRef = useRef<HTMLInputElement>()
+  const queryClient = useQueryClient()
+  const router  = useRouter();
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [newMemoryCardValue, setNewMemoryCardValue] = useState<string>("");
+
+  const createMemoryCardMutate = useMutation({
+    mutationFn: addMemoryCard,
+    onSuccess: data => {
+      queryClient.setQueryData(["memoryCard"], data)
+      queryClient.invalidateQueries(["memoryCard"], {exact: true})
+    }
+  })
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    createMemoryCardMutate.mutate({
+      name: nameRef.current.value
+    })
+    setNewMemoryCardValue("")
+    setModalOpen(false)
+    router.refresh()
+  }
+
+
+  return (
+    <div className="justify-center ml-10 mr-10 mt-5 mb-5">
+      <button
+        onClick={() => setModalOpen(true) }
+        className='btn btn-primary w-full pl-24 pr-24 mb-2.5'
+      >
+        문제 추가하기<AiOutlinePlus size={18} />
+      </button>
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} >
+        <form onSubmit={handleSubmit}>
+          <h3 className="font-bold text-lg">문제 추가하기</h3>
+          <div className="modal-action">
+            <label >
+              <span className="label-text">문제유형</span>
+            </label>
+            <input
+                id="questionType"
+                name="questionType"
+                value={memoryCard?.questionType}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">난이도</span>
+            </label>
+            <input
+                id="level"
+                name="level"
+                value={memoryCard?.level}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">질문</span>
+            </label>
+            <input
+                id="question"
+                name="question"
+                type="text"
+                value={memoryCard?.question}
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">부연 설명</span>
+            </label>
+            <input
+                id="explanation"
+                name="explanation"
+                value={memoryCard?.explanation}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">문항1</span>
+            </label>
+            <input
+                id="num1"
+                name="num1"
+                type="text"
+                value={memoryCard?.num1}
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">문항2</span>
+            </label>
+            <input
+                id="num2"
+                name="num2"
+                value={memoryCard?.num2}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">문항3</span>
+            </label>
+            <input
+                id="num3"
+                name="num3"
+                value={memoryCard?.num3}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">문항4</span>
+            </label>
+            <input
+                id="num4"
+                name="num4"
+                value={memoryCard?.num4}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <label >
+              <span className="label-text">정답</span>
+            </label>
+            <input
+                id="rightAnswer"
+                name="rightAnswer"
+                value={memoryCard?.rightAnswer}
+                type="text"
+                placeholder="Type hear"
+                className="input input-bordered w-full"
+            />
+            <button type="submit" disabled={createCategoryMutate.isLoading} className="btn text-lg w-1/6">
+              {createCategoryMutate.isLoading ? "Loading..." : "Create"}
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddMemoryCard
---
+++ components/MemoryCard/MainMemoryCardList.tsx
@@ -0,0 +1,59 @@
+import {TMemoryCard, TMemoryCards} from "@/types/types"
+import MemoryCard from "./MemoryCard";
+import {QueryClient} from "@tanstack/query-core"
+import axios from "axios";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
+import React  from "react";
+import {getAllMemoryCard} from "@/app/api/memoryCardApi";
+
+interface CategoryProps {
+  memoryCards: TMemoryCards
+}
+
+
+const MemoryCardList = () => {
+
+  const queryClient: QueryClient = useQueryClient();
+
+  const { status, data, error, isFetching, isPreviousData} = useQuery({
+    queryKey: ['memoryCards'],
+    queryFn: () => getAllMemoryCard(),
+    keepPreviousData: true,
+    // cacheTime: 1000,
+    // staleTime: 1000,
+  });
+
+  if (status === "loading") return <h1>Loading...</h1>
+  if (status === "error") {
+    return <h1>{JSON.stringify(error)}</h1>
+  }
+
+
+
+  return (
+    <>
+    <div className="flex-auto m-5">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+        <tr>
+          <th className="text-blue-500 text-lg">질문</th>
+          <th className="text-blue-500 text-lg">실행</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        {data?.list.map((memoryCard) => (
+          <MemoryCard id={memoryCard.id} key={memoryCard.id} mainCategory={memoryCard} />
+        ))}
+        </tbody>
+      </table>
+    </div>
+      {/*<div className="flex justify-center pr-24 pl-24">*/}
+      {/*  <button className='btn btn-primary w-full m-5 mr-5' onClick={() => resetData()} >가져오기</button>*/}
+      {/*</div>*/}
+    </>
+  );
+};
+
+export default MemoryCardList;
